8607dfbeafc34e57aa210c036de555799a80020d
LeonDlugosch <leon.dlugosch@uni-oldenburg.de>
2019-08-13 16:32:08 +0200

    More customization options added
    lty and axes argument will be passed.
    Added option to add regession and points to existing plot.

--- plot_lm.R
+++ plot_lm.R
@@ -1,7 +1,7 @@
 plot_lm = function(resp.data, expl.data, d = 1,
                    main = "", xlab = "Explanatory variable", ylab = "Response variable",
-                   col = "black", bg = "black", l1.col = "red", l2.col = "black", pch = 21, cex = 1,
-                   axes = T, ylim = NULL, xlim = NULL, mod.pos = "front", plot.result = NULL){
+                   col = "black", bg = "black", l1.col = "red", l2.col = "black", pch = 21, cex = 1, las = 2, lty = 2, lwd = 2, 
+                   axes = TRUE, ylim = NULL, xlim = NULL, mod.pos = "front", plot.result = NULL, add = FALSE, predict = TRUE){
   n = length(resp.data)/length(unique(expl.data))
   mod = lm(resp.data ~ poly(expl.data, d, raw = T))
   s.mod = summary(mod) 
@@ -15,13 +15,16 @@ plot_lm = function(resp.data, expl.data, d = 1,
   fit.mod = as.data.frame(predict(mod, interval = "conf"))
   fit.mod$f = expl.data
   fit.mod = fit.mod[order(fit.mod$f),]
-  
-  lm.plot = plot(y=resp.data, x=expl.data, type = "n", xlab = xlab, ylab = ylab, pch = pch, bg = bg, cex = cex, col = col, 
-                 main = paste(main), axes = axes, ylim = ylim, xlim = xlim)
+  if(add == FALSE){
+    lm.plot = plot(y=resp.data, x=expl.data, type = "n", xlab = xlab, ylab = ylab, pch = pch, bg = bg, cex = cex, col = col, 
+                   main = paste(main), axes = axes, ylim = ylim, xlim = xlim, las = las)
+  }
   if(mod.pos == "front"){points(y=resp.data, x=expl.data, pch = pch, bg = bg, cex = cex, col = col)}
-  lines(fit.mod[,"lwr"] ~ fit.mod$f, col = l2.col, lty = 3, lwd = 2)
-  lines(fit.mod[,"upr"] ~ fit.mod$f, col = l2.col, lty = 3, lwd = 2)
-  lines(fit.mod[,"fit"] ~ fit.mod$f, col = l1.col, lty = 2, lwd = 3)
+  if(predict == TRUE){
+    lines(fit.mod[,"lwr"] ~ fit.mod$f, col = l2.col, lty = 3, lwd = 2)
+    lines(fit.mod[,"upr"] ~ fit.mod$f, col = l2.col, lty = 3, lwd = 2)
+  }
+  lines(fit.mod[,"fit"] ~ fit.mod$f, col = l1.col, lty = lty, lwd = 3)
   if(mod.pos == "back"){points(y=resp.data, x=expl.data, pch = pch, bg = bg, cex = cex, col = col)}
   if(!is.null(plot.result)){
     text = paste(paste("adj.r2 = ", round(r,3),", ", sep = ""), paste("p.val ", p, sep = ""))
